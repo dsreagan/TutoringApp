0bdfe26ec11da50b571d527390682bac83a7c9cb
Daniel Reagan <d.reagan.dr@gmail.com>
2023-03-03 17:07:09 -0600

    Added code for db creation and db models. Moved js folder into static folder

--- website/models.py
+++ website/models.py
@@ -1,3 +1,28 @@
 from . import db
 from flask_login import UserMixin
 
+class Student(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(150))
+    last_name = db.Column(db.String(150))
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    phone_number = db.Column(db.String(20))
+    # Not sure if needed
+    appointments = db.relationship('Appointment')
+
+class Tutor(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(150))
+    last_name = db.Column(db.String(150))
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    phone_number = db.Column(db.String(20))
+    # Not sure if needed
+    appointments = db.relationship('Appointment')
+
+class Appointment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime())
+    student_id = db.Column(db.Integer, db.ForeignKey('student.id'))
+    tutor_id = db.Column(db.Integer, db.ForeignKey('tutor.id'))

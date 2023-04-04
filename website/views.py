fe7b02c17f7acbb51bd094aaa945c39402049861
Ally Lawrence <69950668+AllyLawrence@users.noreply.github.com>
2023-04-04 03:01:17 +0000

    Print all tutors on index.html home page

--- website/views.py
+++ website/views.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, render_template
+from .models import *
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def index():
-    return render_template('index.html')
+    tutor_all = Tutor.query.all()
+    return render_template('index.html', tutor_all=tutor_all)

2fe5b1acbd1aa7ea8b9f50a4fc3b17f9404eb606
Ally Lawrence <69950668+AllyLawrence@users.noreply.github.com>
2023-04-18 01:54:45 +0000

    fixed search for tutors, created and styled User and Tutor Home pages. there is a button for both pages on index for tests purposes. Will be deleted once sign up/on is finishe

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, url_for
 from .models import *
 
 views = Blueprint('views', __name__)
@@ -11,3 +11,16 @@ def index():
     else:
         tutor_all = Tutor.query.all()
     return render_template('index.html', tutor_all=tutor_all)
+
+@views.route('/userHome')
+def userHome():
+    tutor_search = request.args.get('tutor_search')
+    if tutor_search:
+        tutor_all = Tutor.query.filter(Tutor.first_name.contains(tutor_search) | Tutor.last_name.contains(tutor_search) | Tutor.id.contains(tutor_search))
+    else:
+        tutor_all = Tutor.query.all()
+    return render_template('userHome.html', tutor_all=tutor_all)
+
+@views.route('/tutorHome')
+def tutorHome():
+    return render_template('tutorHome.html')

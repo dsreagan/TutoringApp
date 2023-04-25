02d5561361fffabedafea533346bf5cee4ecae1c
Ally Lawrence <69950668+AllyLawrence@users.noreply.github.com>
2023-04-25 05:38:58 +0000

    fixed views for index all tutor search and display. Also fixed some styling on student home page

--- website/views.py
+++ website/views.py
@@ -6,29 +6,25 @@ views = Blueprint('views', __name__)
 
 @views.route('/')
 def index():
-    return render_template('index.html', user=current_user)
+    tutor_search = request.args.get('tutor_search')
+    if tutor_search:
+        tutor_all = Tutor.query.filter(Tutor.first_name.contains(tutor_search) | Tutor.last_name.contains(tutor_search) | Tutor.id.contains(tutor_search) | Tutor.subjects.contains(tutor_search)).all()
+    else:
+        tutor_all = Tutor.query.all()
+    return render_template('index.html', user=current_user, tutor_all=tutor_all)
 
 @views.route('/registered')
 def registered():
     return render_template('registered.html')
-
-    tutor_search = request.args.get('tutor_search')
-    if tutor_search:
-        tutor_all = Tutor.query.filter(Tutor.first_name.contains(tutor_search) | Tutor.last_name.contains(tutor_search) | Tutor.id.contains(tutor_search))
-    else:
-        tutor_all = Tutor.query.all()
-    return render_template('index.html', tutor_all=tutor_all)
 
 @views.route('/student')
 @login_required
 def student_home():
     tutor_search = request.args.get('tutor_search')
     if tutor_search:
-        tutor_all = Tutor.query.filter(Tutor.first_name.contains(tutor_search) | Tutor.last_name.contains(tutor_search) | Tutor.id.contains(tutor_search))
+        tutor_all = Tutor.query.filter(Tutor.first_name.contains(tutor_search) | Tutor.last_name.contains(tutor_search) | Tutor.id.contains(tutor_search) | Tutor.subjects.contains(tutor_search)).all()
     else:
         tutor_all = Tutor.query.all()
-
-
     return render_template('student-home.html', user=current_user, tutor_all=tutor_all)
 
 @views.route('/tutor')
